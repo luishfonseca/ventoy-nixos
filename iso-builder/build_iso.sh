9c078eb90b90813b5f77d8fa60e4d880993fb025
Luís Fonseca <luis@lhf.pt>
2024-07-08 02:07:33 +0100

    fix: read gitignored files

--- iso-builder/build_iso.sh
+++ iso-builder/build_iso.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env -S nix shell nixpkgs#bash nixpkgs#rsync --command bash
 
-rsync --progress $(nix build --no-link --print-out-paths)/iso/nixos*.iso ../ISO/nixos.iso
+rsync --progress $(nix build path:. --no-link --print-out-paths)/iso/nixos*.iso ../ISO/nixos.iso
