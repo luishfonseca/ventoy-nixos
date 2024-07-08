0a40ed8fdff44af75ee29559a22740a508923a3c
Luís Fonseca <luis@lhf.pt>
2024-07-08 03:19:50 +0100

    feat: flake-utils and use authorizedKeys file

--- iso-builder/flake.nix
+++ iso-builder/flake.nix
@@ -1,45 +1,56 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: let
-    pkgs = import nixpkgs {system = "x86_64-linux";};
-  in {
-    nixosConfigurations."isoimage" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-        {
-          nix.extraOptions = "experimental-features = nix-command flakes";
-
-          isoImage.contents = [
-            { source = ./ts.key; target = "/ts.key"; }
-            { source = ./ssh_host_ed25519_key; target = "/ssh_host_ed25519_key"; }
-          ];
-
-          services.tailscale = {
-            enable = true;
-            openFirewall = true;
-            authKeyFile = "/iso/ts.key";
-          };
-
-          services.openssh.hostKeys = [];
-          boot.postBootCommands = ''
-            cp /iso/ssh_host_ed25519_key /etc/ssh/
-            chmod 400 /etc/ssh/ssh_host_ed25519_key
-          '';
-
-          services.getty.autologinUser = pkgs.lib.mkForce "root";
-
-          users.users.root.openssh.authorizedKeys.keys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE98aQ0VshDOnylLmZcfEdbuxZllCDtfBYH2786f4nph luis@altair"
-          ];
-        }
-      ];
-    };
-
-    packages.x86_64-linux.default = self.nixosConfigurations.isoimage.config.system.build.isoImage;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = {self, ...} @ inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import inputs.nixpkgs {inherit system;};
+      in {
+        nixosConfigurations.isoimage = inputs.nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+            {
+              nix.extraOptions = "experimental-features = nix-command flakes";
+
+              isoImage.contents = [
+                {
+                  source = ./ts.key;
+                  target = "/ts.key";
+                }
+                {
+                  source = ./ssh_host_ed25519_key;
+                  target = "/ssh_host_ed25519_key";
+                }
+                {
+                  source = ./authorized_keys;
+                  target = "/authorized_keys";
+                }
+              ];
+
+              services.tailscale = {
+                enable = true;
+                openFirewall = true;
+                authKeyFile = "/iso/ts.key";
+              };
+
+              services.openssh.hostKeys = [];
+              boot.postBootCommands = ''
+                cp /iso/ssh_host_ed25519_key /etc/ssh/
+                chmod 400 /etc/ssh/ssh_host_ed25519_key
+              '';
+
+              services.openssh.authorizedKeysFiles = ["/iso/authorized_keys"];
+
+              services.getty.autologinUser = pkgs.lib.mkForce "root";
+            }
+          ];
+        };
+
+        defaultPackage = self.nixosConfigurations.${system}.isoimage.config.system.build.isoImage;
+      }
+    );
 }
